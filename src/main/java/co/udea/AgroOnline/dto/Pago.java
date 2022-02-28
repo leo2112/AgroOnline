683867b213ddd6becf9d56a96e90f630e3a04daa
DESKTOP-KFJE4B7\PC <charlstore@gmail.com>
2022-02-27 21:13:28 -0500

    Se creo la entidad para pago

--- src/main/java/co/udea/AgroOnline/dto/Pago.java
+++ src/main/java/co/udea/AgroOnline/dto/Pago.java
@@ -2,8 +2,56 @@ package co.udea.AgroOnline.dto;
 
 import java.util.Date;
 
+@Entity
+@Table(name = pago)
 public class Pago {
+
+    @Id
+    @GeneratedValue(strategy = Generationatype.IDENTITY)
+    @Colum(name = "idfactura")
+    private Long idfactura;
+
+    @Colum(name = "fecha")
     private Date fecha;
-    private double valor;
+
+    @Colum(name = "descripcion")
     private String descripcion;
+
+    @Colum(name = "total")
+    private double total;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "fk_idfactura", insertable = false, updatable = false)
+
+    public Long getIdfactura() {
+        return idfactura;
+    }
+
+    public void setIdfactura(Long idfactura) {
+        this.idfactura = idfactura;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public double getTotal() {
+        return total;
+    }
+
+    public void setTotal(double total) {
+        this.total = total;
+    }
 }
