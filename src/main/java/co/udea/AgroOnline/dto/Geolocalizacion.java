2d36e73d715eeceacf60efe6637401412ca337df
alcideszapata <alcides.zapata@udea.edu.co>
2022-02-27 21:13:13 -0500

    se crearon las entidades de la clase Geolocalización y se crearon los setter and getter

--- src/main/java/co/udea/AgroOnline/dto/Geolocalizacion.java
+++ src/main/java/co/udea/AgroOnline/dto/Geolocalizacion.java
@@ -1,8 +1,40 @@
 package co.udea.AgroOnline.dto;
 
-public class Geolocalizacion {
-    public String departamento;
-    public String ciudad;
-    public String barrio;
 
+@Entity
+@Table(name = geolocalizacion)
+public class Geolocalizacion extends GoogleMap {
+
+    @Id
+    @GeneratedValue(strategy = Generationatype.IDENTITY)
+    @Colum(name = "departamento")
+    public String departamento;
+    @Colum(name = "ciudad")
+    public String ciudad;
+    @Colum(name = "barrioVereda")
+    public String barrioVereda;
+
+    public String getDepartamento() {
+        return departamento;
+    }
+
+    public void setDepartamento(String departamento) {
+        this.departamento = departamento;
+    }
+
+    public String getCiudad() {
+        return ciudad;
+    }
+
+    public void setCiudad(String ciudad) {
+        this.ciudad = ciudad;
+    }
+
+    public String getBarrioVereda() {
+        return barrioVereda;
+    }
+
+    public void setBarrioVereda(String barrioVereda) {
+        this.barrioVereda = barrioVereda;
+    }
 }
